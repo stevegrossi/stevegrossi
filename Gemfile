e39bc94229cfcb0b1d60c916e3bde9becb5cf03b
Steve Grossi <me@stevegrossi.com>
2011-04-30 10:56:41 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.6'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'mysql2', '~> 0.2.0'
+
+# Bundle the extra gems:
+# gem 'nokogiri'
+# gem 'sqlite3-ruby', :require => 'sqlite3'
+
+gem "jquery-rails"
+gem "rspec-rails", ">= 2.0.1", :group => [:development, :test]
+gem "haml", ">= 3.0.0"
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Stevegrossi::Application.config.session_store :cookie_store, :key => '_stevegrossi_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Stevegrossi::Application.config.session_store :active_record_store
