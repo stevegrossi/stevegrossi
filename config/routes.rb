b86bb84c70f1f31b1b9869f9fc164373bfda21da
Steve Grossi <me@stevegrossi.com>
2011-04-30 11:06:41 -0400

    Pages controller with home and about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Stevegrossi::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
+  get "pages/home"
+
+  get "pages/about"
+
+  root :to => "pages#home"
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
@@ -9,21 +12,6 @@ Stevegrossi::Application.routes.draw do
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
 
   # Sample resource route with sub-resources:
   #   resources :products do
@@ -39,20 +27,8 @@ Stevegrossi::Application.routes.draw do
   #     end
   #   end
 
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
 
   # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
 end
