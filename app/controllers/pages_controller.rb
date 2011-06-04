91f87dae0655b5fb74cad2a6702cf52147e623d2
Steve Grossi <me@stevegrossi.com>
2011-06-04 08:15:43 -0400

    Created home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   def home
+    @body_class = 'home'
+    
+    @latest_work = '';
+    @latest_writing = Writing.find(:first, :order => "created_at DESC");
+    @latest_book = Book.find(:first, :order => "created_at DESC")
   end
 
   def about
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,8 @@ module ApplicationHelper
     raw md.to_html
   end
   
+  def get_cover(book)
+    image_tag("http://images.amazon.com/images/P/#{ book.asin }.01.LZZZZZZZ.jpg", :alt => book.title, :class => 'cover')
+  end
+  
 end
