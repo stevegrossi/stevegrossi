d4266fa4eb0035f8f503bd1708a214b74abfeb1e
Steve Grossi <me@stevegrossi.com>
2011-08-24 21:50:37 -0400

    Add evening styles between 6pm and 6am

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -10,4 +10,11 @@ $(function() {
   four = 'mail';
   $('#contact').html('<a accesskey="9" href="'+four+'to:'+one+'@'+two+'.'+three+'">' + one+'@'+two+'.'+three+'</a>');
 
+  // Hook for evening styles
+  var hr = (new Date()).getHours();
+  if (hr > 18 || hr < 6) {
+    $('body').addClass('evening');
+    $('.home h1').html('Good evening, I\'m Steve and I build things on the internet for people. I\'m interested in just what the hell is going on, among other things. This, like me, is a work-in-progress.')
+  }
+
 });
