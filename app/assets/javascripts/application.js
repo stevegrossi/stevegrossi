aeb16057d4a19012e59be50c4847e979ba0072cd
Steve Grossi <me@stevegrossi.com>
2012-05-04 07:42:44 -0400

    Fluid grid

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,7 +19,8 @@ $(function() {
   var hr = (new Date()).getHours();
   if (hr > 17 || hr < 6) {
     $('body').addClass('evening');
-    $('.home h1').html('Good evening, I\'m Steve and I build things on the internet for people. I\'m interested in just what the hell is going on, among other things. This, like me, is a work-in-progress.');
+    var greeting = $('.home h1');
+    greeting.text( greeting.text().replace("Hey", "Good evening") );
   }
 
 });
