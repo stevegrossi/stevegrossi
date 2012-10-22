0afff9590507ad479dd032bd881054da14f1a79e
Steve Grossi <me@stevegrossi.com>
2012-10-22 17:38:48 -0400

    Change body classes to "ctrlr ctrlr-action"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,47 +1,48 @@
 module ApplicationHelper
-  
+
   def title
     @title ? @title + ' | Steve Grossi' : 'Steve Grossi'
   end
-  
+
   def description
     @description ? @description : 'From the website of Steve Grossi'
   end
-  
+
   def markdown(text)
     raw text.blank? ? '' : Maruku.new(text).to_html
   end
-  
+
   def strip_markdown(text)
     strip_tags(markdown(text))
   end
-  
+
   def body_classes
-    [ controller_path.sub('/', ' '), # so 'meta/books' becomes 'meta books'
-      action_name,
+    controller_slug = controller_path.sub('/', ' ') # so 'meta/books' becomes 'meta books'
+    [ controller_slug,
+      "#{controller_slug}-#{action_name}",
       Date::DAYNAMES[Time.now.wday].downcase
     ].join ' '
   end
-  
+
   def link_to_new(class_name)
     if current_user
       link_to "New #{class_name}", new_polymorphic_path(class_name), :class => 'link_to_new'
     end
   end
-  
+
   def link_to_edit(thing)
     if current_user
       link_to 'e', polymorphic_path(thing, :action => :edit), :class => 'edit_link'
     end
   end
-  
+
   def link_to_delete(thing)
     if current_user
       name = thing.class.name.titleize
       link_to "Delete this #{name}", polymorphic_path(thing), :confirm => "Are you sure you want to delete this #{name}? This cannot be undone.", :method => :delete, :class => 'delete_link'
     end
   end
-  
+
   def link_to_previous(thing)
     if thing.previous
       str = content_tag :span, 'Previous: '
@@ -49,7 +50,7 @@ module ApplicationHelper
       return content_tag :p, str, :class => :previous
     end
   end
-  
+
   def link_to_next(thing)
     if thing.next
       str = content_tag :span, 'Next: '
@@ -57,7 +58,7 @@ module ApplicationHelper
       return content_tag :p, str, :class => :next
     end
   end
-  
+
   def nav_link_to(*args)
     options = args.extract_options!
     path = Rails.application.routes.recognize_path args[1]
@@ -73,15 +74,15 @@ module ApplicationHelper
     end
     link_to args[0], path, options
   end
-  
+
   def nice_url(url)
     url.gsub(/(^https?:\/\/(www.)?)|(\/$)/, '')
   end
-  
+
   def link_from_url(url)
     link_to nice_url(url), url
   end
-  
+
   def flash_errors(obj)
     if obj.errors.any?
       content_tag :div, :class => 'flash error' do
@@ -94,11 +95,11 @@ module ApplicationHelper
       end
     end
   end
-  
+
   def word_count(str)
     pluralize( str.gsub(/[^-a-zA-Z]/, ' ').split.size, 'word' )
   end
-  
+
   def mark_string(string, term='')
     if term.blank?
       string
@@ -112,7 +113,7 @@ module ApplicationHelper
     first_occurrence = query.split(' ').first
     mark_string(excerpt(strip_markdown(text), first_occurrence), query)
   end
-  
+
   def comment_on_search_term(query)
     unless query.blank?
       case query.downcase
@@ -121,10 +122,10 @@ module ApplicationHelper
       end
     end
   end
-  
+
   def bubble_up(array, items)
     if items.blank?
-      return array 
+      return array
     else
       (array - items).unshift(items).flatten
     end
