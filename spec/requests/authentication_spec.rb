c65a46ad61d6d2d4f4791afe37956e99df635c51
Steve Grossi <me@stevegrossi.com>
2012-10-29 11:22:22 -0400

    Test for "already logged in" flash

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -2,24 +2,23 @@ require 'spec_helper'
 
 describe "Authentication" do
 
-  let(:user) { FactoryGirl.create(:user) }
+  before :each do
+    log_in_user
+  end
 
   it 'logs in a user' do
-    visit log_in_path
-    fill_in 'Username', with: user.username
-    fill_in 'Password', with: 'secret'
-    click_button 'Log In'
     page.should have_content('Hey there, sexy')
     current_path.should == meta_dashboard_path
   end
 
   it 'logs out a user' do
-    visit log_in_path
-    fill_in 'Username', with: user.username
-    fill_in 'Password', with: 'secret'
-    click_button 'Log In'
     visit log_out_path
     page.should have_content('See you around, cowboy')
     current_path.should == root_path
+  end
+
+  it 'notifies a user if they are already logged in' do
+    visit log_in_path
+    page.should have_content('already logged in')
   end
 end
