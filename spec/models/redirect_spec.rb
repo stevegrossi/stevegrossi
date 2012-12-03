0db883c946777ee99190dcfd7c2ce9b642f18ea0
Steve Grossi <me@stevegrossi.com>
2012-12-03 17:42:17 -0500

    Annotate redirect_spec.rb

--- spec/models/redirect_spec.rb
+++ spec/models/redirect_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: redirects
+#
+#  id         :integer         not null, primary key
+#  from       :string(255)
+#  to         :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
 require 'spec_helper'
 
 describe Redirect do
