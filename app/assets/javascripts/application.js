f91a4826c78a94d98da67371006048e0bf2c3a70
Steve Grossi <me@stevegrossi.com>
2013-02-24 09:38:06 -0500

    Inline contact link obfuscation
    Thanks, hivelogic.com/enkoder/

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,13 +5,5 @@
 //= require_self
 
 $(function() {
-
   $('html').removeClass('no-js').addClass('js');
-
-  one = 'me';
-  two = 'stevegrossi';
-  three = 'com';
-  four = 'mail';
-  $('#contact').html('<a accesskey="9" href="'+four+'to:'+one+'@'+two+'.'+three+'">' + one+'@'+two+'.'+three+'</a>');
-
 });
