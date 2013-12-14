e4df96afbc1209baae63d2b4e701e79002f6d248
Steve Grossi <me@stevegrossi.com>
2013-12-14 10:03:42 -0500

    No need to check if redirect.to exists (it's req'd)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
       @page = Page.find_by_permalink!(params[:id])
     rescue ActiveRecord::RecordNotFound
       redirect = Redirect.find_by_from!(params[:id])
-      redirect_to redirect.to if redirect
+      redirect_to redirect.to
     end
   end
 
