9c3d010694030e66a9b3b50a3c4da5ad61e76ef8
Steve Grossi <me@stevegrossi.com>
2014-03-22 12:55:34 -0400

    Use bigfoot for footnotes
    Nice work! http://www.bigfootjs.com/

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,13 @@
 //= require jquery
 //= require jquery_ujs
-//= require footnotes
 //= require jquery.lazyload
 //= require respond
+//= require jquery.bigfoot
 //= require_self
 
 $(function() {
   $('html').removeClass('no-js').addClass('js');
+
+  var bigfoot = $.bigfoot();
+  bigfoot.addBreakpoint('iPhone');
 });
