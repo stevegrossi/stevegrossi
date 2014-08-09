ff2fa4d5c522b4e4ab675a0c746e57a644cb6ea3
Steve Grossi <me@stevegrossi.com>
2014-08-09 07:57:06 -0400

    Small refactor of redirect finder

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,7 @@ class PagesController < ApplicationController
     begin
       @page = Page.find_by_permalink!(params[:id])
     rescue ActiveRecord::RecordNotFound
-      redirect = Redirect.find_by_from!(params[:id])
-      redirect_to redirect.to
+      redirect_to Redirect.find_by_from!(params[:id]).to
     end
   end
 
