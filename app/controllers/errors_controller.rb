1fdc909d68c9b88fb202602c910eec0ae6f0eb07
Steve Grossi <me@stevegrossi.com>
2014-08-09 09:29:59 -0400

    Simplify errors_controller (no more unique formats)

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,19 +1,14 @@
 class ErrorsController < ApplicationController
   def error_404
-    respond_to do |format|
-      format.html { render status: 404 }
-      format.any  { render text: "404 Not Found", status: 404 }
-    end
+    render status: 404
   end
 
   def error_422
-    respond_to do |format|
-      format.html { render status: 422 }
-      format.any  { render text: "422 Unprocessable Entity", status: 422 }
-    end
+    render status: 422
   end
 
   def error_500
-    render file: "#{Rails.root}/public/500.html", layout: false, status: 500
+    render layout: false, status: 500,
+           file: Rails.root.join('/public/500.html')
   end
 end
