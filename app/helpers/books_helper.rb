0e98a80a4173f92e595c0c4cb7e17371f41cca6f
Steve Grossi <me@stevegrossi.com>
2014-08-09 10:41:58 -0400

    Test and refactor BooksHelper

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,22 +1,12 @@
 module BooksHelper
 
   def linked_author_list(book)
-    author_links = []
-    book.authors.each do |author|
-      author_links << link_to(author.full_name, author)
-    end
-    raw author_links.to_sentence
+    book.authors.map{ |author|
+      link_to(author.full_name, author)
+    }.to_sentence
   end
 
   def author_list(book)
     book.authors.map(&:full_name).to_sentence
-  end
-
-  def books_by(author)
-    book_links = []
-    author.books.each do |book|
-      book_links << link_to(book.title, book)
-    end
-    raw book_links.to_sentence
   end
 end
---
+++ spec/helpers/books_helper_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe BooksHelper do
+
+  describe '#author_list' do
+    it 'lists authors' do
+      author1 = build(:author)
+      author2 = build(:author)
+      book = build(:book_without_author, authors: [author1, author2])
+      expected = "#{author1.full_name} and #{author2.full_name}"
+      author_list(book).should eq(expected)
+    end
+  end
+
+  describe '#linked_author_list' do
+    it 'lists authors linked to their pages' do
+      author1 = build(:author)
+      author2 = build(:author)
+      book = build(:book_without_author, authors: [author1, author2])
+      expected = link_to(author1.full_name, author1)
+      expected += ' and '
+      expected += link_to(author2.full_name, author2)
+      linked_author_list(book).should eq(expected)
+    end
+  end
+end
