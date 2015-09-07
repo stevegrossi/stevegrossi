e9eab1dc58de7814474a6defb009e350192ed48b
Steve Grossi <me@stevegrossi.com>
2015-09-06 21:16:46 -0400

    Add specs for User#days_alive
    Thanks for the tip, CodeClimate!

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,20 @@
+require "spec_helper"
+
+describe User do
+
+  describe "#days_alive" do
+    let(:user) { build(:user, birthdate: 1.year.ago.to_date) }
+
+    it "returns the number of days since the user’s date of birth" do
+      expect(user.days_alive).to be 365
+    end
+
+    context "when the user has no birthday" do
+      let(:user) { build(:user) }
+
+      it "returns 0" do
+        expect(user.days_alive).to be 0
+      end
+    end
+  end
+end
